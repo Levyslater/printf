da440d3e1701e1ba3fccce9e970c8070d54f1ed4
annahmuthoniM <annahmuthoni@93gmail.com>
2023-07-23 19:51:37 -0700

    printf6.c

---
+++ printf6.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * get_size - get size
+ * @format: Format string
+ * @b:type 
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *b)
+{
+	int curr_b = *b + 1;
+	int size = 0;
+
+	if (format[curr_b] == 'l')
+		size = S_LONG;
+	else if (format[curr_b] == 'j')
+		size = S_SHORT;
+
+	if (size == 0)
+		*b = curr_b - 1;
+	else
+		*b = curr_b;
+
+	return (size);
+}
